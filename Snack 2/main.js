137fb59d977cb74bea6745d2ecd3af0c8b0bc220
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-28 00:31:48 +0200

    Inizio snack 2

--- Snack 2/main.js
+++ Snack 2/main.js
@@ -5,4 +5,53 @@
 // Punti fatti e falli subiti.
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console
 
+let squadreCalcio = [
+
+{
+    'nome' : 'Roma',
+    'punti fatti' : 0,
+    'falli subiti' : 0,
+},
+
+{
+    'nome' : 'Juventus',
+    'punti fatti' : 0,
+    'falli subiti' : 0,
+},
+
+{
+    'nome' : 'Fiorentina',
+    'punti fatti' : 0,
+    'falli subiti' : 0,
+},
+
+{
+    'nome' : 'Foggia',
+    'punti fatti' : 0,
+    'falli subiti' : 0,
+},
+
+{
+    'nome' : 'Termoli',
+    'punti fatti' : 0,
+    'falli subiti' : 0,
+},
+
+{
+    'nome' : 'Bari',
+    'punti fatti' : 0,
+    'falli subiti' : 0,
+}
+
+]
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max )
+}
+
+for (let i = 0; i < squadreCalcio.length; i++) {
+    let falli = getRandomInt(40);
+    
+    
+}
 
