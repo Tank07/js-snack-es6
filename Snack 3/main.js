81d039197224a1ef1d4f59cd69577f518dd8dbe2
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-28 23:56:08 +0200

    Primo step esercizio 4

--- Snack 3/main.js
+++ Snack 3/main.js
@@ -3,3 +3,6 @@
 // e dati due numeri min e max (min più piccolo di max).
 // Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
 // Eseguiamo questo esercizio prima con forEach e poi con filter.
+
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+
